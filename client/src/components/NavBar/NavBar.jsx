fd491e12ad62d1f4e111437ad4cf066d7aafd8ce
Kacem <sarah.kacem2@dxc.com>
2022-06-03 10:00:44 +0200

    navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import {BottomNavigationAction,BottomNavigation} from '@mui/material/';
+import {BottomNavigationAction,BottomNavigation,Typography} from '@mui/material/';
 import FolderIcon from '@mui/icons-material/Folder';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import HomeIcon from '@mui/icons-material/Home';
+import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
 
-export default function LabelBottomNavigation() {
+
+export default function NavBar() {
   const [value, setValue] = React.useState('recents');
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
@@ -13,6 +15,23 @@ export default function LabelBottomNavigation() {
 
   return (
     <BottomNavigation sx={{ width: 100/100 }} value={value} onChange={handleChange}>
+        <VideogameAssetIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+         <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 100,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 800,
+              letterSpacing: '.3rem',
+              color: 'success',
+              textDecoration: 'none',
+            }}
+          >SNES
+          </Typography>
       <BottomNavigationAction
         label="Menu"
         value="recents"
@@ -20,7 +39,7 @@ export default function LabelBottomNavigation() {
       />
 
       <BottomNavigationAction
-        label="My Collection"
+        label="Add"
         value="nearby"
         icon={<AddCircleIcon />}
       />
